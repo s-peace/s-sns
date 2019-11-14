6a0d3c4be7f05de875dc72c071e9909b6de6ca4b
s-peace <ssmim777@gmail.com>
2019-11-14 15:33:55 +0900

    add_model_user

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,3 @@
 class Chat < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
   root to: 'chats#index'
   resources :chats
+  resources :users
 end
